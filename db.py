74016c55623db619d0db417690d5b0332a1f440b
DanielSanchezDiaz <47653762+DanielSanchezDiaz@users.noreply.github.com>
2021-03-23 04:17:38 -0400

    Update db.py

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ from flask_pymongo import pymongo
 from app import app
 
 client = pymongo.MongoClient(
-    "mongodb+srv://DanielSanchez:SafeLink@topdomainnames.mj0ts.mongodb.net/DomainNames?retryWrites=true&w=majority",
+    "mongodb+srv://<nice try>:<nope>@topdomainnames.mj0ts.mongodb.net/DomainNames?retryWrites=true&w=majority",
     ssl=True, ssl_cert_reqs='CERT_NONE')
 db = client.get_database('DomainNames')
 domainNames = db.get_collection('TopDomainNames')
