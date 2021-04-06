d6e59f94936f0d12b20783f7d766171071dd6e02
Obed-Ababio <43188128+Obed-Ababio@users.noreply.github.com>
2021-04-06 00:28:09 -0400

    combo should be working now

--- algorithms/combosquatting/combosquatting.py
+++ algorithms/combosquatting/combosquatting.py
@@ -1,5 +1,6 @@
 from db import getAllDomains
 from urllib.parse import urlparse
+import tldextract
 
 def find_combosquatting(url):
     
@@ -24,6 +25,12 @@ def is_substring(false_domain_name, minimal_str_len = 4,source_list='database'):
         domain_dict = getAllDomains()
         print(domain_dict)
 
+        for entry in domain_dict:
+            domain = tldextract.extract(entry['domain']).domain
+            if domain in false_domain_name \
+                and domain != false_domain_name \
+                and domain >= minimal_str_len:
+                    matches.append(domain)
     # combare with trademarks in file
     else:
         path =\
