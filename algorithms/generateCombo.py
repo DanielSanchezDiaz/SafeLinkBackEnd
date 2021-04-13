98ca89bb4f60c7cbcca0df1901ffb40ee6974523
Obed-Ababio <43188128+Obed-Ababio@users.noreply.github.com>
2021-04-12 23:42:17 -0400

    add local file database and made generateCombo use only local files instead of databse

--- algorithms/generateCombo.py
+++ algorithms/generateCombo.py
@@ -1,5 +1,4 @@
 from urllib.parse import urlparse
-from SafeLinkBackEnd import db
 import tldextract
 
 def find_combosquatting(url):
@@ -11,11 +10,11 @@ def find_combosquatting(url):
     domain = urlparse(url).netloc
     false_trademark = domain.split('.')[1]
     
-    print(f'DOMAIN: {domain} FALSE: {false_trademark}')
-    return is_substring(false_trademark, 4,'database')
+    print(f'DOMAIN: {domain} \nFALSE: {false_trademark}')
+    return is_substring(false_trademark, 4)
 
 
-def is_substring(false_domain_name, minimal_str_len = 4,source_list='database'):
+def is_substring(false_domain_name, minimal_str_len = 4):
     """
     Takes a false domain name and scans a file or database to check
     for registered trademarks of which the the false domain name is a substring and
@@ -24,35 +23,24 @@ def is_substring(false_domain_name, minimal_str_len = 4,source_list='database'):
     """
 
     matches = []
-    # compare with trademarks on database
-    if source_list == 'database':
-        domain_dict = db.getAllDomains()
-        print(domain_dict)
-
-        for entry in domain_dict:
-            domain = tldextract.extract(entry['domain']).domain
-            if domain in false_domain_name \
-                and domain != false_domain_name \
-                and len(domain) >= minimal_str_len:
-                    matches.append(domain)
     # combare with trademarks in file
-    else:
-        filename = source_list
-        path =\
-             '/Users/obedababio/Documents/Spring 2021/CPSC 490/SafeLink/SafeLinkBackEnd/algorithms/combosquatting/' + filename
-        custom_file = open(path, 'r')
-        lines = custom_file.readlines()
-        for string in lines:
-            trademark_name = tldextract.extract(string).domain
-            if trademark_name in false_domain_name \
-            and trademark_name != false_domain_name:
-                matches.append(trademark_name)
+    path =\
+            '/Users/obedababio/Documents/Spring 2021/CPSC 490/SafeLink/SafeLinkBackEnd/file_database/trademarks.txt'
+    custom_file = open(path, 'r')
+    lines = custom_file.readlines()
+    for string in lines:
+        #trademark_name = tldextract.extract(string).domain
+        trademark_name = string.rstrip()
+        print(f'is {trademark_name} in {false_domain_name}?')
+        if trademark_name in false_domain_name and trademark_name != false_domain_name:
+            matches.append(trademark_name)
+
     matches = set(matches)
     return list(matches)
 
 
 
 
-print(find_combosquatting('http://www.somethingfacebooksomething.test/foo/bar'))
+print(find_combosquatting('http://www.somethingpepsisomething.test/foo/bar'))
 
 
