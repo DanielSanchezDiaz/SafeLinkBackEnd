642461fa5ed951a4f7e715cff10cf548073473e7
Danny Sanchez <daniel.sanchez@yale.edu>
2021-04-26 01:05:00 -0400

    update

--- checks_hub.py
+++ checks_hub.py
@@ -4,8 +4,9 @@ from requests import request
 from flask import jsonify
 import tldextract
 import db
-
-
+import requests
+import datetime
+from algorithms.generateSound import Homophones
 
 
 def typo_squatting(url, json_response_dict):
@@ -14,22 +15,15 @@ def typo_squatting(url, json_response_dict):
     possible typosquatted domains from the url and adds a key-value
     pair in the form {"typoSquatting": "[...]" }
     """
-    typoModel = ts_models()
-    typos = typoModel.generate_ts_domains("google.com")
-    print(typos.values())
-    links = request.json
-    firstLink = links[0].lower()
-    parts = tldextract.extract(firstLink)
+    parts = tldextract.extract(url)
     cl_domain = [parts.domain, '.' + parts.suffix]
     f_clean_domain = ''.join(cl_domain)
     result = db.queryTypoSquat(f_clean_domain)
-
-    # if result:
-    #     return jsonify("Typosquat detected!")
-    # else:
-    #     return jsonify("Coast is clear!")
-
+    if result:
+        json_response_dict["STATUS"] = "FAILED"
+    json_response_dict["typoSquatting"] = result
     # ADD RESULT TO DICT HERE
+
 
 def combo_squatting(url, json_response_dict):
     """
@@ -38,13 +32,58 @@ def combo_squatting(url, json_response_dict):
     pair in the form {"comboSquatting": "[...]" }
     """
     combo_list = find_combosquatting(url)
+    if combo_list:
+        json_response_dict["STATUS"] = "FAILED"
+    json_response_dict["comboSquatting"] = combo_list
     # add to dict
 
 
+def sound_squatting(url, json_response_dict):
+    """
+    takes in a url and json object, generates a list of
+    possible soundsquatted domains from the url and adds a key-value
+    pair in the form {"comboSquatting": "[...]" }
+    """
+    parts = tldextract.extract(url)
+    cl_domain = [parts.domain, '.' + parts.suffix]
+    f_clean_domain = ''.join(cl_domain)
+    result = db.querySoundSquat(f_clean_domain)
+    if result:
+        json_response_dict["STATUS"] = "FAILED"
+    json_response_dict["soundSquatting"] = result
 
 
+def detect_new_domains(url, json_response_dict):
+    # Things to do
+        # cache result of rdap query, with date you got it
+        # check in database if you have record for this already
 
-def main_security_check_module(url):
+    #parts = tldextract.extract(url)
+    #cl_domain = [parts.domain, '.' + parts.suffix]
+    #f_clean_domain = ''.join(cl_domain)
+    f_clean_domain = "youtube.com"
+    info = requests.get("https://www.rdap.net/domain/"+f_clean_domain).json()
+    # This should be the registration date
+    events = info["events"]
+    response = []
+    numDays = 30
+    for event in events:
+        if event['eventAction'] == 'registration':
+            date = event['eventDate']
+            day, time = date.split('T')
+            year, month, day = day.split('-')
+            registrationDate = datetime.date(int(year), int(month), int(day))
+            currentDate = datetime.datetime.date(datetime.datetime.now())
+            diff = currentDate - registrationDate
+            # one month is reasonable
+            # make days a parameter
+            if diff.days < numDays:
+                json_response_dict['STATUS'] = 'FAILED'
+                response = f"This domain was registered {diff.days} ago" #display something with this on frontend
+    json_response_dict['New Domain'] = response
+
+
+def main_security(url):
     """
     takes a url and passes the url to different
     modules each checking for different
@@ -66,7 +105,6 @@ def main_security_check_module(url):
 
     typo_squatting(url, json_response_dict)
     combo_squatting(url, json_response_dict)
+    sound_squatting(url, json_response_dict)
+    detect_new_domains(url, json_response_dict)
     return json_response_dict
-
-
-
--- db.py
+++ db.py
@@ -1,11 +1,6 @@
-import ssl
-import urllib
-
-import certifi
-from flask import Flask
 from flask_pymongo import pymongo
-from app import app
-from generateTypo import ts_models
+from algorithms.generateTypo import ts_models
+from algorithms.generateSound import Homophones
 from tranco import Tranco
 
 client = pymongo.MongoClient(
@@ -16,17 +11,21 @@ db = client.get_database('DomainNames')
 
 def getTopDomains():
     '''Returns a list of the top domains'''
-    t = Tranco(cache=True, cache_dir='.tranco')
+    t = Tranco(cache=False, cache_dir='.tranco')
     latest_list = t.list().top(10000)
     return latest_list
+
 
 def upDateDataBase():
     topDomainsCol = db.get_collection('TopDomainNames')
     domains = getTopDomains()
     i = 1
     typoCol = db.get_collection('TypoSquats')
+    soundcol = db.get_collection('SoundSquats')
     typoModel = ts_models()
+    homoModel = Homophones('file_database/homophone_list', 'file_database/wordlists/wordsEn.txt')
     typoId = 1
+    homoId = 1
     for domain in domains:
         entry = {"_id": str(i), "domain": domain}
         topDomainsCol.save(entry)
@@ -38,6 +37,15 @@ def upDateDataBase():
                 typoCol.save(entry)
                 typoId += 1
         # *******************************
+        homos = homoModel.find_h_domains_single(domain)
+        for squat in homos["soudsquatting_single"]:
+            entry = {"_id": str(homoId), "squat": squat[0], "domain": domain}
+            soundcol.save(entry)
+            homoId += 1
+        for squat in homos["soudsquatting_double"]:
+            entry = {"_id": str(homoId), "squat": squat[0], "domain": domain}
+            soundcol.save(entry)
+            homoId += 1
         i += 1
 
 
@@ -50,3 +58,9 @@ def queryTypoSquat(link):
 def getAllDomains():
     topDomainsCol = db.get_collection('TopDomainNames')
     return list(topDomainsCol.find({}))
+
+
+def querySoundSquat(link):
+    soundCol = db.get_collection('SoundSquats')
+    result = soundCol.find_one({'squat': link})
+    return result
